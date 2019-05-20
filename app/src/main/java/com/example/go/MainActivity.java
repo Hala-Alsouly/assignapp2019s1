621e942d40008ec892467a5bc4f7ed6f7b0e9933
Jun Yang <jun.yang1@anu.edu.au>
2019-05-20 13:46:37 +1000

    Simple Welcome Page

--- app/src/main/java/com/example/go/MainActivity.java
+++ app/src/main/java/com/example/go/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.go;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    /**
+     * Write the onClick for the three buttons
+     */
+    public void onClick_VsCom(View v){
+
+    }
+    public void onClick_VsPlayer(View v){
+
+    }
+    public void onClick_CheckRecords(View v){
+
     }
 }
